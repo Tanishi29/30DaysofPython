298fd9481f52ece18c274319b71a9c49b156b0c1
Tanishi29 <tanishikumar12@gmail.com>
2026-07-14 19:38:13 -0700

    day 1 learning python

---
+++ Day01/helloworld.py
@@ -0,0 +1,37 @@
+
+import math
+
+#single lined comment
+
+"""
+multi lined comment"""
+
+
+print(3 + 4)
+print(3 - 4)
+print(3*4)
+print(3 / 4)
+print(3 // 4)
+print(3 ** 4)
+print(3 % 4)
+
+print("Tanishi")
+print("Kumar")
+print("US")
+print("Iam enjoying learning python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Tanishi', 'Python', 'Finland']))
+print(type('Tanishi'))
+print(type(True))
+print(type({'country':'India'}))
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print(math.sqrt((8**2) + (5**2)))
+print(math.sqrt((10-2)**2 + (8-3)**2))
+
+
